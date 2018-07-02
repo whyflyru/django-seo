b8403f21084fa510603cf0de86f57389800e1e39
Greesha <grigoriymikhalkin@gmail.com>
2018-07-02 19:41:32 +0300

    qf

--- tests/userapp/urls.py
+++ tests/userapp/urls.py
@@ -2,12 +2,13 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
+from .views import page_detail, product_detail, tag_detail, my_view, my_other_view
 
 
 urlpatterns = [
-    url(r'^pages/([\w\d-]+)/', 'userapp.views.page_detail', name="userapp_page_detail"),
-    url(r'^products/(\d+)/', 'userapp.views.product_detail', name="userapp_product_detail"),
-    url(r'^tags/(.+)/', 'userapp.views.tag_detail', name="userapp_tag"),
-    url(r'^my/view/(.+)/', 'userapp.views.my_view', name="userapp_my_view"),
-    url(r'^my/other/view/(.+)/', 'userapp.views.my_other_view', name="userapp_my_other_view"),
+    url(r'^pages/([\w\d-]+)/', page_detail, name="userapp_page_detail"),
+    url(r'^products/(\d+)/', product_detail, name="userapp_product_detail"),
+    url(r'^tags/(.+)/', tag_detail, name="userapp_tag"),
+    url(r'^my/view/(.+)/', my_view, name="userapp_my_view"),
+    url(r'^my/other/view/(.+)/', my_other_view, name="userapp_my_other_view"),
 ]
