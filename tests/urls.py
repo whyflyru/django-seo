5e404cb8b3d33915b67476e46d741da89d493bbe
Greesha <grigoriymikhalkin@gmail.com>
2018-07-02 19:32:48 +0300

    qf

--- tests/urls.py
+++ tests/urls.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
-from django.urls import patterns, include
+from django.urls import include
 from django.contrib import admin
 from userapp.admin import alternative_site
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^alt-admin/', include(alternative_site.urls)),
     url(r'^', include('userapp.urls')),
-)
+]
--- tests/userapp/urls.py
+++ tests/userapp/urls.py
@@ -2,13 +2,12 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
-from django.urls import patterns
 
 
-urlpatterns = patterns('userapp.views',
-    url(r'^pages/([\w\d-]+)/', 'page_detail', name="userapp_page_detail"),
-    url(r'^products/(\d+)/', 'product_detail', name="userapp_product_detail"),
-    url(r'^tags/(.+)/', 'tag_detail', name="userapp_tag"),
-    url(r'^my/view/(.+)/', 'my_view', name="userapp_my_view"),
-    url(r'^my/other/view/(.+)/', 'my_other_view', name="userapp_my_other_view"),
-)
+urlpatterns = [
+    url(r'^pages/([\w\d-]+)/', 'userapp.views.page_detail', name="userapp_page_detail"),
+    url(r'^products/(\d+)/', 'userapp.views.product_detail', name="userapp_product_detail"),
+    url(r'^tags/(.+)/', 'userapp.views.tag_detail', name="userapp_tag"),
+    url(r'^my/view/(.+)/', 'userapp.views.my_view', name="userapp_my_view"),
+    url(r'^my/other/view/(.+)/', 'userapp.views.my_other_view', name="userapp_my_other_view"),
+]
