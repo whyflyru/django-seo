26ba8bf79e0fc77db1c47c3fcc5b2febe2e8a8bb
Dmitry Shulyatnikov <dshulyatnikov@gmail.com>
2018-10-22 15:28:50 +0300

    pep 440 for version

--- djangoseo/version.py
+++ djangoseo/version.py
@@ -1,17 +1,2 @@
 # -*- coding:utf-8 -*-
-VERSION = (2, 5, '0wf', 'final', 0)
-
-
-def get_version():
-    global VERSION
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    elif VERSION[3] != 'final':
-        version = '%s %s %s' % (version, VERSION[3], VERSION[4])
-    return version
-
-
-__version__ = get_version()
+__version__ = "2.5.0+whyfly.1"
