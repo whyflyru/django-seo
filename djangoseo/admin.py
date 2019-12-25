116eec2ca0af974173814819b4b905b432582e44
Sergey <s.stepennov@whyfly.ru>
2019-12-25 16:34:21 +0300

    improvements

--- djangoseo/admin.py
+++ djangoseo/admin.py
@@ -80,7 +80,7 @@ def get_view_admin(use_site=False, use_subdomains=False):
     })
 
 
-def register_seo_admin(admin_site, metadata_class, list_filter=None):
+def register_seo_admin(admin_site, metadata_class, filter_list=None):
     """ Register the backends specified in Meta.backends with the admin """
     use_sites = metadata_class._meta.use_sites
     use_subdomains = metadata_class._meta.use_subdomains
@@ -100,7 +100,7 @@ def register_seo_admin(admin_site, metadata_class, list_filter=None):
         class ModelAdmin(model_admin):
             form = get_model_form(metadata_class)
             list_display = model_admin.list_display + get_list_display()
-            list_filter = list_filter
+            list_filter = filter_list
 
         _register_admin(admin_site, metadata_class._meta.get_model('model'), ModelAdmin)
 
@@ -108,7 +108,7 @@ def register_seo_admin(admin_site, metadata_class, list_filter=None):
         class ViewAdmin(view_admin):
             form = get_view_form(metadata_class)
             list_display = view_admin.list_display + get_list_display()
-            list_filter = list_filter
+            list_filter = filter_list
 
         _register_admin(admin_site, metadata_class._meta.get_model('view'), ViewAdmin)
 
@@ -116,7 +116,7 @@ def register_seo_admin(admin_site, metadata_class, list_filter=None):
         class PathAdmin(path_admin):
             form = get_path_form(metadata_class)
             list_display = path_admin.list_display + get_list_display()
-            list_filter = list_filter
+            list_filter = filter_list
 
         _register_admin(admin_site, metadata_class._meta.get_model('path'), PathAdmin)
 
@@ -124,7 +124,7 @@ def register_seo_admin(admin_site, metadata_class, list_filter=None):
         class ModelInstanceAdmin(model_instance_admin):
             form = get_modelinstance_form(metadata_class)
             list_display = model_instance_admin.list_display + get_list_display()
-            list_filter = list_filter
+            list_filter = filter_list
 
         _register_admin(admin_site, metadata_class._meta.get_model('modelinstance'), ModelInstanceAdmin)
 
